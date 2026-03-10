13174c2ce9ade63dae6ac4ad51097396526a0041
farrel24si <farrel24si@mahasiswa.pcr.ac.id>
2026-03-10 15:42:38 +0700

    First commit

---
+++ src/pertemuan-2/HelloWorld.jsx
@@ -0,0 +1,54 @@
+export default function HelloWorld(){
+    const propsUserCard = {
+        nama: "Goku",
+        nim: "999999",
+        tanggal: "2025-01-01"
+    }
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>Selamat Belajar ReactJs</p>
+            <GreetingBinjai/><br/>
+            <GreetingBinjai/>
+            <QuoteText/>
+            <UserCard
+            nama = "farrel"
+            nim = "2457301052"
+            tanggal = "2026/03/10"/>
+            <UserCard
+            nama = "kj"
+            nim = "123"
+            tanggal = "2026/03/10"/>
+            <UserCard {...propsUserCard}/> 
+        </div>
+    )
+}
+
+function GreetingBinjai(){
+    return (
+        <small className="card">Salam dari binjai 🤣</small>
+    )
+}
+
+function UserCard(props){
+    return (
+        <div className="card">
+            <hr/>
+            <h3>Nama: {props.nama}</h3>
+            <p>NIM: {props.nim}</p>
+            <p>Tanggal: {props.tanggal}</p>
+        </div>
+    )
+}
+
+function QuoteText() {
+    const text = "Mulutmu Harimaumu";
+    const text2 = "Aku ingin jadi macan😘";
+    return (
+        <div>
+            <hr/>
+            <p>{text.toLowerCase()}</p>
+            <p>{text2.toUpperCase()}</p>
+        </div>
+    )
+}
